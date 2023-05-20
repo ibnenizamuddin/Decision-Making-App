3d3f7916a62f1677118340f25f21c3d78d1115e5
Ehsanullah Shaikh <ibnenizamuddin@gmail.com>
2023-05-20 23:48:32 +0530

    Finish Second Page
    Finish Second Page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 app.config['listofitems']=[]
+app.config['listoffactor']=[]
 
 
 @app.route("/")
@@ -19,11 +20,63 @@ def additems():
             print(app.config['listofitems'])
             app.config['listofitems'].append(item)
             return render_template("index.html",errormessage="",listofitems=app.config['listofitems'])
-
         else:
             return render_template("index.html",errormessage="You have added maximum Number of Items, please proceed ahead by click on 'Next'",listofitems=app.config['listofitems'])
 
+@app.route("/pageAction", methods=['GET','POST'])
+def firstPageActions():
+    if request.method=='POST':
+        submitType=request.form.get('submitType')
+        if submitType=='reset':
+            app.config['listofitems']=[]
+            app.config['listoffactor']=[]
+            return render_template("index.html",errormessage="",listofitems=app.config['listofitems'])
+        if submitType=='next':
+            return render_template("nextpage.html",errormessage="",listoffactor=app.config['listoffactor'])
 
+
+
+@app.route("/nextpage.html")
+def nextpage():
+    return render_template("nextpage.html",errormessage="",listoffactor=app.config['listoffactor'])
+
+@app.route("/pageAction2", methods=['GET','POST'])
+def secondPageActions():
+    if request.method=='POST':
+        submitType=request.form.get('submitType')
+        if submitType=='back':
+            return render_template("index.html",errormessage="",listofitems=app.config['listofitems'])
+        if submitType=='next':
+            return render_template("finalpage.html",errormessage="",listoffactor=app.config['listoffactor'])
+
+
+
+@app.route("/createfactors", methods=['GET','POST'])
+def addfactors():
+    if request.method=='POST':
+        if len(app.config['listoffactor'])<5:
+            item=request.form.get('factoritems')
+            print(app.config['listoffactor'])
+            app.config['listoffactor'].append(item)
+            return render_template("nextpage.html",errormessage="",listoffactor=app.config['listoffactor'])
+        else:
+            return render_template("nextpage.html",errormessage="You have added maximum Number of Items, please proceed ahead by click on 'Next'",listoffactor=app.config['listoffactor'])
+
+
+@app.route("/pageAction3", methods=['GET','POST'])
+def finalPageActions():
+    if request.method=='POST':
+        submitType=request.form.get('submitType')
+        if submitType=='back':
+            return render_template("nextpage.html",errormessage="",listoffactor=app.config['listoffactor'])
+        if submitType=='calculate':
+            return render_template("finalpage.html",errormessage="Calculating",listoffactor="")
+
+
+
+@app.route("/finalpage.html")
+def finalpage():
+    return render_template("finalpage.html",errormessage="",listoffactor=app.config['listoffactor'])
 
 
 
